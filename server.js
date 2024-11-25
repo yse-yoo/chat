6ca2fc0ed02d1fad6174744487268b04242c104a
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-11-26 08:46:42 +0900

    update

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
+
 // Expressアプリケーションの作成
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "http://localhost", // クライアントのオリジンを許可
+        origin: "*",
         methods: ["GET", "POST"],
         allowedHeaders: ["my-custom-header"],
         credentials: true
@@ -19,10 +23,9 @@ io.on('connection', (socket) => {
     console.log('ユーザーが接続しました');
 
     // クライアントからのメッセージを受信
-    socket.on('chat message', (msg) => {
-        console.log('メッセージ受信: ' + msg);
-        socket.broadcast.emit('chat message', msg);
-        // 全クライアントにメッセージをブロードキャスト
+    socket.on('message', (msg) => {
+        console.log('メッセージ受信: ', msg);
+        socket.broadcast.emit('message', msg); // 全クライアントにオブジェクト形式でブロードキャスト
     });
 
     socket.on('disconnect', () => {
@@ -31,6 +34,6 @@ io.on('connection', (socket) => {
 });
 
 // サーバーを起動
-server.listen(3000, () => {
-    console.log('Socket.IOサーバーがポート3000で起動しました');
+server.listen(PORT, () => {
+    console.log(`Socket.IOサーバーが起動しました: http://${HOST}:${PORT}`);
 });
